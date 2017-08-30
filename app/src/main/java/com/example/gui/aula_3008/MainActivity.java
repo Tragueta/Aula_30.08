57be8f1c5921e7d685ebd0b457e94c834a302136
Tragueta <guilherme.tragueta@hotmail.com>
2017-08-30 19:50:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gui/aula_3008/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gui.aula_3008;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
